54f0e096383e96ab0445022292a0324fa9cf6bb5
csfwn <chesafwan1997@gmail.com>
2023-02-13 15:37:54 +0800

    Add project files.

---
+++ BlazorFullStackCrud/Server/Migrations/20230213063149_Second.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorFullStackCrud.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MyProperty_Comics_ComicId",
+                table: "MyProperty");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty");
+
+            migrationBuilder.RenameTable(
+                name: "MyProperty",
+                newName: "SuperHeroes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MyProperty_ComicId",
+                table: "SuperHeroes",
+                newName: "IX_SuperHeroes_ComicId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SuperHeroes",
+                table: "SuperHeroes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SuperHeroes_Comics_ComicId",
+                table: "SuperHeroes",
+                column: "ComicId",
+                principalTable: "Comics",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SuperHeroes_Comics_ComicId",
+                table: "SuperHeroes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SuperHeroes",
+                table: "SuperHeroes");
+
+            migrationBuilder.RenameTable(
+                name: "SuperHeroes",
+                newName: "MyProperty");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SuperHeroes_ComicId",
+                table: "MyProperty",
+                newName: "IX_MyProperty_ComicId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MyProperty_Comics_ComicId",
+                table: "MyProperty",
+                column: "ComicId",
+                principalTable: "Comics",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
